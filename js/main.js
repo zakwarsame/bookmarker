adbd57ce693c2b4501d4cf6786dc59ac5994f9d9
zealchemy <zakatu2018@gmail.com>
2018-10-18 19:57:20 -0400

    adding regex functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ function saveBookmark(e) {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
+
+  if(!siteName || !siteUrl){
+    alert("Please fill in the form");
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -67,4 +76,22 @@ function fetchBookmarks() {
 
 
   }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
 }
